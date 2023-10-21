d31d8183ec9b3d5db57900d94f1543fb987d13fa
sonikasuman <sumansonika1002@gmail.com>
2023-10-21 19:04:06 +0530

    basic thread class commit

--- first-java/multithreading/src/main/java/Main.java
+++ first-java/multithreading/src/main/java/Main.java
@@ -1,17 +1,32 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            Runnable r = () -> {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("hello " + i + " " + Thread.currentThread().getName());
+                }
+            };
+            Thread t1 = new Thread(r, "thread 1");
+            Thread t2 = new Thread(r, "thread 2");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            t1.start();
+            t2.start();
+            System.out.println("started");
+            t1.interrupt();
+            t1.join();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+
+            System.out.println("completed 1");
+            t2.join();
+            System.out.println("completed 2");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+
     }
+
 }
